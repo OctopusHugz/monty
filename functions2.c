d57a3aa27453a1ce05f6f2d6cb541beceac622d2
OctopusHugz <colsonscott53@gmail.com>
2020-05-14 05:39:39 +0000

    Added nop function and outline for add function to new file functions2.c. Added both to monty.h header file.

---
+++ functions2.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * add - adds top two elements of the stack
+ * @stack: stack to add from
+ * @line_number: line number of nop opcode
+ **/
+void add(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	printf("Running the add function\n");
+}
+
+/**
+ * nop - does nothing
+ * @stack: stack to do nothing from
+ * @line_number: line number of nop opcode
+ **/
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
