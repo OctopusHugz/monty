7d02ac36c044a8d8119a8c3217c3ad6dc87b21a7
OctopusHugz <colsonscott53@gmail.com>
2020-05-15 06:18:20 +0000

    Updated div logic to perform right division

--- functions2.c
+++ functions2.c
@@ -70,14 +70,14 @@ void divide(stack_t **stack, unsigned int line_number)
 		return;
 	}
 
-	quotient = (*stack)->n;
+	quotient = (*stack)->next->n;
 	if (quotient == 0)
 	{
 		dprintf(STDERR_FILENO, "L%d: division by zero\n", line_number);
 		global = -1;
 		return;
 	}
-	quotient /= (*stack)->next->n;
+	quotient /= (*stack)->n;
 	(*stack)->next->n = quotient;
 	pop(stack, line_number);
 }
